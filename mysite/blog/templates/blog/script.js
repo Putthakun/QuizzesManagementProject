2d1a8f45acda71ce5c35dcd31bfecc6989e08128
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-10-26 21:00:24 +0700

    add popup and rename file css
    cuz file css error

--- mysite/blog/templates/blog/script.js
+++
@@ -1,6 +0,0 @@
-const showPopup = document.querySelector('.box_end_test');
-const popupContainer = document.querySelector('.popup_container');
-
-showPopup.onclick = () => {
-    popupContainer.classList.add('active');
-}
