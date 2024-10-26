417c478e4c3845f7220732131f18f8d7716844f9
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-10-26 23:00:20 +0700

    add popup on home teacher page

---
+++ mysite/blog/static/blog/js/popup_teacher.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const showPopup = document.querySelector('.box_add_subject');
+    const popupContainer = document.querySelector('.popup_container');
+    const popupClose = document.querySelector('.popup_box_right_tail_btn');
+
+    console.log("showPopup:", showPopup);  // ตรวจสอบค่า showPopup
+    console.log("popupContainer:", popupContainer);  // ตรวจสอบค่า popupContainer
+    console.log("popupClose:", popupClose);  // ตรวจสอบค่า popupContainer
+
+    if (showPopup && popupContainer) {
+        showPopup.onclick = () => {
+            popupContainer.classList.add('active');
+        };
+    } else {
+        console.error("Not found!!!");
+    }
+
+    if (popupContainer && popupClose) {
+        popupClose.onclick = () => {
+            popupContainer.classList.remove('active');
+        };
+    }
+});
+
+
